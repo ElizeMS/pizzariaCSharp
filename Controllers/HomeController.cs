573a0dfa578788d3210e3300e2d39a330fcb0400
Elize Mariane <elizemariane1234@gmail.com>
2025-06-06 20:45:35 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using projeto.Models;
+using System.Diagnostics;
+using System.Runtime.Intrinsics.X86;
+
+
+namespace projeto.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Cardapio()
+        {
+            return View();
+            
+            
+        }
+        public IActionResult Pedido()
+        {
+            return View();
+           
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
